48654384546c127226ef779d5053b4624f5d5402
Delay37 <benettiescritorio@gmail.com>
2025-02-21 19:00:54 -0300

    Completa o exercicio2, usa o mesmo css do ex1

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ function Soma() {
     document.getElementById("divresposta").style.border="gray solid"
     divrespostajs.innerHTML = ("A soma é igual a: "+ resposta);
 }
+
+function SomaSemBotao() {
+    let numero1js = Number(document.getElementById("numero1").value);
+    let numero2js = Number(document.getElementById("numero2").value);
+    let numero3js = Number(document.getElementById("numero3").value);
+
+    let resposta = numero1js + numero2js + numero3js;
+
+    let divrespostajs = document.getElementById("divresposta");
+    document.getElementById("divresposta").style.display="block"
+    document.getElementById("divresposta").style.border="gray solid"
+    divrespostajs.innerHTML = ("A soma é igual a: "+ resposta);
+}
